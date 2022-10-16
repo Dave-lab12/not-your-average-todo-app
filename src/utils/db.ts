6b0cb3bdd530e01aef19cf553ebc88f724c90d5a
dave <dwtmekonnen123@gmail.com>
2022-10-16 16:30:13 +0300

    feat: with prettier

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,17 +1,17 @@
-import * as mongoose from "mongoose";
-import {config} from "./config";
-import {logger} from "./logger";
+import * as mongoose from 'mongoose';
+import { config } from './config';
+import { logger } from './logger';
 
 export async function connectToDb() {
-    try {
-        await mongoose.connect(config.DATABASE_URL)
-        logger.info("connected to database")
-    } catch (e) {
-        logger.error(e)
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(config.DATABASE_URL);
+    logger.info('connected to database');
+  } catch (e) {
+    logger.error(e);
+    process.exit(1);
+  }
 }
 
 export function disconnectDb() {
-    return mongoose.connection.close();
+  return mongoose.connection.close();
 }
